716bb64809d934113af163755390c05147089d60
lucasllabanca <lucasllabanca@hotmail.com>
2022-09-25 14:33:55 -0300

    feat: cards created from pokeapi from 1 to 151

--- js/pokedex.js
+++ js/pokedex.js
@@ -1,28 +1,37 @@
-// async function fetchData(pokeNumber) {
-//     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeNumber}`)
-//     const pokemon = await response.json()
-//     return pokemon
-// }
+async function getPokemonByNumber(pokemonNumber) {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonNumber}`)
+    return await response.json()
+}
 
-// async function createPokemon(pokeNumber) {
-//     const pokemon = await fetchData(pokeNumber)
-//     const div = document.createElement('div')
-//     const h2 = document.createElement('h2')
-//     const img = document.createElement('img')
-//     h2.textContent = `#${pokemon.order} - ${pokemon.name}`
-//     img.src = pokemon.sprites.front_default
-//     div.appendChild(h2)
-//     div.appendChild(img)
-//     document.body.appendChild(div)
-// }
+async function createPokemon(pokemonNumber) {
+    const pokemon = await getPokemonByNumber(pokemonNumber)
 
-// async function getPokemons() {
-//     for (i = 1; i <= 500; i++) {
-//         await createPokemon(i)
-//     }
-// }
-//const pokeNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    const div = document.createElement('div')
+    const header = document.createElement('header')
+    const footer = document.createElement('footer')
+    const h2 = document.createElement('h2')
+    const img = document.createElement('img')
+    const span = document.createElement('span')
+    div.className = `card ${pokemon.types[0].type.name}`
+    h2.innerText = `#${pokemon.order}`
+    img.src = pokemon.sprites.front_default
+    img.alt = pokemon.name
+    footer.className = pokemon.types[0].type.name
+    span.innerText = pokemon.name
+    header.appendChild(h2)
+    footer.appendChild(span)
+    div.appendChild(header)
+    div.appendChild(img)
+    div.appendChild(footer)
+    return div
+}
 
-//pokeNumbers.forEach(createPokemon)
+async function getPokemons() {
+    const pokedex = document.getElementById('pokedex')
+    const pokemonNumbers = Array.from(new Array(151), (x, i) => i + 1);
+    for (let pokemonNumber of pokemonNumbers) {
+        pokedex.appendChild(await createPokemon(pokemonNumber))
+    }
+}
 
 getPokemons()
